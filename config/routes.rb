a0a50db271e00d44138bb4dd872beebfea3858cd
Bianca Ferreira <biancaferreiralp@gmail.com>
2020-08-25 21:26:38 -0300

    Installed redcarped and emd gems, implemented the show method (route, controller, view) to display the task's details and styled the view

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  
+  # READ
   get 'tasks', to: 'tasks#index'
+  get 'tasks/:id', to: 'tasks#show', as: 'task'
 end
